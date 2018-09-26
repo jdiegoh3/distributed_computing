53c759af80003b8e966599cfd41469eb56550167
jdiegoh3 <jdiegoh3@gmail.com>
2018-09-25 23:13:40 -0500

    Added timer to the datetime parsed from the NTP. Missing implementation of the sockets to retrieve the datetime from the clients

--- exercise14/server.py
+++ exercise14/server.py
@@ -2,32 +2,46 @@ import threading
 import datetime
 import time
 import pytz
+import os
 import ntplib
 
 
-def timer():
-    seconds = 0
-    while True:
-        time.sleep(1)
-        seconds = seconds+1
-        print(seconds)
+class TimeFromServer(object):
+    time = None
+
+    def __init__(self, actual_time):
+        self.time = actual_time
+
+    def add(self, timedelta):
+        self.time = self.time + timedelta
+
+    def get_time(self):
+        return self.time
 
 
 def main():
-    ntp = ntplib.NTPClient()
-    # Provide the respective ntp server ip in below function
-    response = ntp.request('0.south-america.pool.ntp.org')
 
-    datetime_server = datetime.datetime.fromtimestamp(response.tx_time, pytz.timezone('America/Bogota'))
+    timer_thread = threading.Thread(target=timer)
+    timer_thread.start()
 
-    utc = datetime.datetime.utcnow().astimezone(pytz.utc)
 
-    # Verify TimeZone are to UTC-5
-    utc_offset = utc - datetime_server
-    print(utc_offset)
-    # timer_thread = threading.Thread(target=timer)
-    # timer_thread.start()
+def timer():
+    while True:
+        datetime_server.add(datetime.timedelta(seconds=1))
+        print(datetime_server.get_time())
+        time.sleep(1)
+        os.system("cls")
 
 
 if __name__ == '__main__':
+    ntp = ntplib.NTPClient()
+    # Provide the respective ntp server ip in below function
+    response = ntp.request('3.south-america.pool.ntp.org')
+
+    datetime_server = TimeFromServer(datetime.datetime.fromtimestamp(response.tx_time, pytz.timezone('America/Bogota')))
+
+    # utc = datetime.datetime.utcnow().astimezone(pytz.utc)
+
+    # Verify TimeZone are to UTC-5
+    # utc_offset = utc - datetime_server
     main()
